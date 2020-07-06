1677e8178b05c3a5d67771e939508ec8a35cd216
Davide Francavilla <davfrancavilla@gmail.com>
2020-07-06 17:31:04 +0200

    Aggiunti punti di domanda

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-var name = prompt ('Qual è il tuo nome');
-var lastName = prompt ('Qual è il tuo cognome');
-var age = prompt ('Qual è la tua età');
+var name = prompt ('Qual è il tuo nome?');
+var lastName = prompt ('Qual è il tuo cognome?');
+var age = prompt ('Qual è la tua età?');
 
 while  ((age / 1) != age) {
   alert("Inserire un numero");
-  var age = prompt("Qual è la tua età");
+  var age = prompt("Qual è la tua età?");
 }
 
 document.getElementById("text").innerHTML = name + lastName + age;
