ffb26a0cfe1bfd84fb3ecd61a9e5273ff4a33c8a
Davide Francavilla <davfrancavilla@gmail.com>
2020-07-06 14:40:19 +0200

    Aggiunto controllo per l'età

--- js/script.js
+++ js/script.js
@@ -2,4 +2,10 @@ var name = prompt ('Qual è il tuo nome');
 var lastName = prompt ('Qual è il tuo cognome');
 var age = prompt ('Qual è la tua età');
 
-document.getElementById('text').innerHTML = name + lastName + age;
+while  ((age / 1) != age) {
+  alert("Inserire un numero");
+  var age = prompt("Qual è la tua età");
+}
+
+document.getElementById("text").innerHTML = name + lastName + age;
+
