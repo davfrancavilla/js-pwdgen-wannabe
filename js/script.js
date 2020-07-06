53272cf405e870e20e569d811f7ac0d2401c0c10
Davide Francavilla <davfrancavilla@gmail.com>
2020-07-06 14:20:35 +0200

    Generatore password pronto

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var name = prompt ('Qual è il tuo nome');
+var lastName = prompt ('Qual è il tuo cognome');
+var age = prompt ('Qual è la tua età');
+
+document.getElementById('text').innerHTML = name + lastName + age;
